6ab7398f4dd0ca9197bd2b5aa6ff10514455768d
purificant <purificant@users.noreply.github.com>
2018-12-13 18:06:28 +0000

    run black formatter

--- setup.py
+++ setup.py
@@ -4,9 +4,7 @@ from Cython.Build import cythonize
 
 
 libsodium = Extension(
-    name="libsodium",
-    sources=["paseto/crypto/libsodium.pyx"],
-    libraries=["sodium"],
+    name="libsodium", sources=["paseto/crypto/libsodium.pyx"], libraries=["sodium"]
 )
 
 setup(name="python-paseto", ext_modules=cythonize([libsodium]))
