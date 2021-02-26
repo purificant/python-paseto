fb95443e71aa3349c4e9714a2f4b23f3dfca21a8
purificant <purificant@users.noreply.github.com>
2021-02-26 21:29:22 +0000

    add docblocks

--- tests/protocol/test_version2.py
+++ tests/protocol/test_version2.py
@@ -1,3 +1,5 @@
+""" This module contains tests for version2 protocol implementation. """
+
 import pytest
 from nacl.bindings.crypto_sign import crypto_sign_seed_keypair, crypto_sign_SEEDBYTES
 
@@ -9,6 +11,7 @@ from paseto.protocol.version2 import Version2
 class TestVersion2(object):
     @pytest.mark.parametrize("footer", [b"", b"baz"])
     def test_encrypt_decrypt(self, footer: bytes) -> None:
+        """ Check that decrypt() reverses encrypt(). """
         message = b"foo"
         key = b"0" * 32
 
@@ -18,6 +21,7 @@ class TestVersion2(object):
 
     @pytest.mark.parametrize("footer", [b"", b"some_footer"])
     def test_sign_verify(self, footer: bytes) -> None:
+        """ Check that verify() reverses sign(). """
         keys = crypto_sign_seed_keypair(b"\x00" * crypto_sign_SEEDBYTES)
 
         message = b"foo"
@@ -28,28 +32,35 @@ class TestVersion2(object):
         assert Version2.verify(signed, public_key, footer) == message
 
     def test_get_nonce(self) -> None:
+        """ Check that nonce can be retrieved. """
         nonce = Version2.get_nonce(b"", b"")
         assert len(nonce) == Version2.NONCE_SIZE
 
     def test_decrypt_invalid_footer(self) -> None:
+        """ Check that exception is raised when footer is not valid during decrypt(). """
         with pytest.raises(InvalidFooter):
             Version2.decrypt(b"header.message.footer", b"a key", b"some_other_footer")
 
     def test_decrypt_invalid_header(self) -> None:
+        """ Check that exception is raised when header is not valid. """
         with pytest.raises(InvalidHeader):
             Version2.decrypt(b"some_incorrect_header.message.footer", b"a key")
 
     def test_verify_footer_success(self) -> None:
+        """ Check footer validation. """
         Version2.check_footer(b"message." + b64(b"footer"), b"footer")
 
     def test_verify_footer_exception(self) -> None:
+        """ Check that exception is raised when footer is not valid during footer validation. """
         with pytest.raises(InvalidFooter):
             Version2.check_footer(b"some message", b"some footer")
 
     def test_verify_header_success(self) -> None:
+        """ Check header verification. """
         Version2.check_header(b"header.message.footer", b"header")
 
     def test_verify_header_exception(self) -> None:
+        """ Check that exception is raised when header is not valid during header validation. """
         with pytest.raises(InvalidHeader):
             Version2.check_header(b"some_header.message.footer", b"other_header")
 
@@ -67,4 +78,5 @@ class TestVersion2(object):
     def test_decode_message(
         self, message: bytes, header: bytes, expected: bytes
     ) -> None:
+        """ Check message decoding. """
         assert Version2.decode_message(message, len(header)) == expected
