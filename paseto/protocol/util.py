a4110853d9ccb01b1971a85c381a6d397cbda1e4
purificant <purificant@users.noreply.github.com>
2021-02-26 12:44:31 +0000

    amend variable names

--- paseto/protocol/util.py
+++ paseto/protocol/util.py
@@ -24,22 +24,22 @@ def le64(num: int) -> bytes:
     return pack("<Q", num)
 
 
-def b64(b: bytes) -> bytes:
+def b64(input_bytes: bytes) -> bytes:
     """Returns base64 encoding.
 
     Input is encoded using base64url as defined in RFC4648, without "=" padding.
     """
     # https://tools.ietf.org/html/rfc4648#section-4
     # remove padding
-    return urlsafe_b64encode(b).rstrip(b"=")
+    return urlsafe_b64encode(input_bytes).rstrip(b"=")
 
 
-def b64decode(b: bytes) -> bytes:
+def b64decode(input_bytes: bytes) -> bytes:
     """ Returns base64 decoding by reversing b64(). """
-    return urlsafe_b64decode(b + b"=" * padding_size(len(b)))
+    return urlsafe_b64decode(input_bytes + b"=" * padding_size(len(input_bytes)))
 
 
-def padding_size(n: int) -> int:
+def padding_size(num: int) -> int:
     """ Calculates base64 padding size as per RFC4648. """
     # according to https://tools.ietf.org/html/rfc4648#section-4
     # only three cases can exist
@@ -49,7 +49,7 @@ def padding_size(n: int) -> int:
         3: 1,  # one padding character
     }
 
-    lookup = n % 4
+    lookup = num % 4
 
     if lookup not in mapping:
         raise ValueError("Impossible input size, unable to calculate padding")
