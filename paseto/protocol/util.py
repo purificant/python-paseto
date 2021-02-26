c5263a16898ede4f68b81d10f83130aec475d0fa
purificant <purificant@users.noreply.github.com>
2021-02-26 12:07:11 +0000

    add more docstrings to utility functions

--- paseto/protocol/util.py
+++ paseto/protocol/util.py
@@ -1,3 +1,5 @@
+""" This module contains utility functions necessary for protocol implementation. """
+
 from base64 import urlsafe_b64decode, urlsafe_b64encode
 from struct import pack
 from typing import List
@@ -5,6 +7,7 @@ from typing import List
 
 # specification: https://tools.ietf.org/html/draft-paragon-paseto-rfc-00#section-2.2.1
 def pae(pieces: List[bytes]) -> bytes:
+    """ Applies Pre-Authentication Encoding (PAE) to input. """
 
     if not isinstance(pieces, List):
         raise TypeError("Expecting a list of bytes-like objects")
@@ -17,20 +20,27 @@ def pae(pieces: List[bytes]) -> bytes:
 
 
 def le64(num: int) -> bytes:
+    """ Encodes a 64-bit unsigned integer into a little-endian binary string. """
     return pack("<Q", num)
 
 
 def b64(b: bytes) -> bytes:
+    """Returns base64 encoding.
+
+    Input is encoded using base64url as defined in RFC4648, without "=" padding.
+    """
     # https://tools.ietf.org/html/rfc4648#section-4
     # remove padding
     return urlsafe_b64encode(b).rstrip(b"=")
 
 
 def b64decode(b: bytes) -> bytes:
+    """ Returns base64 decoding by reversing b64(). """
     return urlsafe_b64decode(b + b"=" * padding_size(len(b)))
 
 
 def padding_size(n: int) -> int:
+    """ Calculates base64 padding size as per RFC4648. """
     # according to https://tools.ietf.org/html/rfc4648#section-4
     # only three cases can exist
     mapping = {
