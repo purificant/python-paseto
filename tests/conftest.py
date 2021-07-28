94586cd072878facac2dea239d0e9387e4061b2e
purificant <purificant@users.noreply.github.com>
2021-07-29 00:43:42 +0100

    implement test fixtures for latest test vectors

---
+++ tests/conftest.py
@@ -0,0 +1,28 @@
+"""This module contains test fixtures."""
+
+import json
+import os
+
+import pytest
+
+
+@pytest.fixture
+def get_test_vectors_v2() -> dict:
+    """Return spec for v2 test vectors."""
+    return get_test_vector("v2")
+
+
+def get_test_vector(version: str) -> dict:
+    """Return deserialised json."""
+    with open(get_test_vector_path(version)) as json_file:
+        return json.load(json_file)
+
+
+def get_test_vector_path(version: str) -> str:
+    """Return path to json file containing test vectors."""
+    return os.path.join(
+        os.path.dirname(os.path.dirname(__file__)),
+        "submodules",
+        "paseto-test-vectors",
+        f"{version}.json",
+    )
