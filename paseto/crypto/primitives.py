b3912a7ce339d9e14bb656e7b6799ace56066dc3
purificant <purificant@users.noreply.github.com>
2021-08-03 11:57:08 +0100

    cleanup imports

--- paseto/crypto/primitives.py
+++ paseto/crypto/primitives.py
@@ -2,9 +2,8 @@
 
 # from nacl.bindings import crypto_aead_xchacha20poly1305_ietf_decrypt as decrypt
 # from nacl.bindings import crypto_aead_xchacha20poly1305_ietf_encrypt as encrypt
-
-from pysodium import crypto_aead_xchacha20poly1305_ietf_encrypt as encrypt
 from pysodium import crypto_aead_xchacha20poly1305_ietf_decrypt as decrypt
+from pysodium import crypto_aead_xchacha20poly1305_ietf_encrypt as encrypt
 from pysodium import crypto_sign_detached as sign
 from pysodium import crypto_sign_verify_detached as verify
 
