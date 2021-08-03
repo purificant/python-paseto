2591674e3460ec40920f41a6f84769252e8b4b8d
purificant <purificant@users.noreply.github.com>
2021-08-03 11:08:57 +0100

    optimise crypto primitives

--- paseto/crypto/primitives.py
+++ paseto/crypto/primitives.py
@@ -1,12 +1,12 @@
 """ This module exports third party primitives. """
 
-from nacl.bindings import crypto_aead_xchacha20poly1305_ietf_decrypt as decrypt
-from nacl.bindings import crypto_aead_xchacha20poly1305_ietf_encrypt as encrypt
+# from nacl.bindings import crypto_aead_xchacha20poly1305_ietf_decrypt as decrypt
+# from nacl.bindings import crypto_aead_xchacha20poly1305_ietf_encrypt as encrypt
+
+from pysodium import crypto_aead_xchacha20poly1305_ietf_encrypt as encrypt
+from pysodium import crypto_aead_xchacha20poly1305_ietf_decrypt as decrypt
 from pysodium import crypto_sign_detached as sign
 from pysodium import crypto_sign_verify_detached as verify
-
-# from pysodium import crypto_aead_xchacha20poly1305_ietf_encrypt as encrypt
-# from pysodium import crypto_aead_xchacha20poly1305_ietf_decrypt as decrypt
 
 # from libsodium import sign, verify, decrypt, encrypt
 
